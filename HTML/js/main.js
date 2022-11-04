9f85e7dfc06614ae29462860c9dc40b8729ca09f
Dimensionnnnn <84111957+Dimensionnnnn@users.noreply.github.com>
2022-11-04 18:09:37 +0600

    Added a script
    Added a script that displays the cost of cryptocurrencies in real time

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -4,3 +4,25 @@ const nav = document.querySelector('.nav');
 btn.addEventListener('click', () =>{
     nav.classList.toggle('menu-open');
 });
+
+var btc = document.getElementById("bitcoin");
+var apc = document.getElementById("apecoin");
+var eth = document.getElementById("ethereum");
+var ltc = document.getElementById("litecoin")
+
+var liveprice = {
+    "async" : true,
+    "scrossDomain": true,
+    "url" : "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Capecoin%2Cethereum%2Clitecoin&vs_currencies=usd",
+
+    "method": "GET",
+    "headers": {}
+}
+
+$.ajax(liveprice).done(function(response) {
+    console.log(response);
+    btc.innerHTML = response.bitcoin.usd;
+    apc.innerHTML = response.apecoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    ltc.innerHTML = response.litecoin.usd;
+});
